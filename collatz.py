6503a29a7a8f1ac6896b6ea87d6308e3be2d2b74
TylerWitt <tylerwittt@yahoo.com>
2014-06-27 12:56:12 -0700

    Create collatz.py
    This is a python function used to analyze the Collatz algorithm.

---
+++ collatz.py
@@ -0,0 +1,16 @@
+# Tyler Witt
+# 6.27.14
+# collatz.py
+# ver 1.0
+
+# This is a python function used to analyze the Collatz algorithm.
+
+num = int(input("Enter a number: "))
+step = 0
+while num != 1:
+  if num % 2 == 0:
+    num = num / 2
+  else:
+    num = num * 3 + 1
+  step = step + 1
+print(step)
